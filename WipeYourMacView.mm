091dc298b5f7087b6d456eaf8136a15e9425b4de
Kenta Murata <mrkn@mrkn.jp>
2011-02-18 23:26:29 +0900

    (c) Kenta Murata.

--- WipeYourMacView.mm
+++ WipeYourMacView.mm
@@ -3,7 +3,7 @@
 //  WipeYourMac
 //
 //  Created by Kenta Murata on 11/02/18.
-//  Copyright 2011 株式会社ジェネティックラボ. All rights reserved.
+//  Copyright 2011 Kenta Murata. All rights reserved.
 //
 
 #import "WipeYourMacView.h"
